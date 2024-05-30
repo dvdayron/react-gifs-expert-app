54d373c07a867d18a36f90d8c39646ee46b322b3
Dayron Ramida Coll <dvdayron@gmail.com>
2024-05-30 09:12:41 -0500

    update enviroment vars

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,7 @@
+import { giphyApiKey } from '../enviroment.env'
+
 export const getGifs = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=JySA48E5TbPBYhDGX8y1fSnQXp91P1LX&q=${category}&limit=12`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=${category}&limit=12`
     const response = await fetch(url)
     const { data = [] } = await response.json()
     const gifs = data.map((gif) => ({
